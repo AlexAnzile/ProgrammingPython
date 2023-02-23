fb911136f598836c766f2b90eaee4021cf19bff4
AlexAnzile <96250355+AlexAnzile@users.noreply.github.com>
2023-02-23 23:18:33 +0100

    Delete ES_CASA directory

--- ES_CASA/Lezione_2/es2.py
+++
@@ -1,7 +0,0 @@
-def sum_list(my_list):
-    s=0
-    if my_list == []:
-        return None
-    for item in my_list:
-        s=s+item
-    return s
--- ES_CASA/Lezione_7/Test/test_es7.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from es7 import somma 
-
-class TestSomma(unittest.TasteCase):
-
-    def test_somma(self):
-        self.asserEqual(somma(1,1), 2)
-        self.asserEqual(somma(1.5,2.5), 4)
